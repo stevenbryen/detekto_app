79cc6fb3a53489c42416ed7e218a59012faae04d
Bryen <sbryen@3c22fb5e337a.ant.amazon.com>
2020-10-01 13:04:01 +0100

    typo in ssh config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -47,10 +47,10 @@ server 'localhost', user: 'deploy', roles: %w{app db web}
 #    auth_methods: %w(password)
 #  }
 
-ssh_options: {
-keys: %w("/home/deploy/.aws/letsbuild-euw1.pem"),
-forward_agent: true,
-auth_methods: %w(publickey)
+set :ssh_options, {
+    keys: %w("/home/deploy/.aws/letsbuild-euw1.pem"),
+    forward_agent: true,
+    auth_methods: %w(publickey)
 }
 #
 # The server-based syntax can be used to override options:
